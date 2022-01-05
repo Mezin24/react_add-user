1a81119d359418247a017c0d066d882800d3ae5b
Pavel Mezencev <mezencev24@mail.ru>
2022-01-05 16:02:40 +0300

    add card module

--- src/User/AddUser.js
+++ src/User/AddUser.js
@@ -1,10 +1,14 @@
-import './AddUser.css';
+import classes from './AddUser.module.css';
 import Card from '../UI/Card';
 
 const AddUser = () => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <Card>
-      <form>
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
         <label htmlFor='name'>Username</label>
         <input type='text' id='name' />
         <label htmlFor='age'>Age (Years)</label>
